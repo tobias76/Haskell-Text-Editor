da9a0e4592dd5b5a83529920f43a597270725a54
Toby <tobyreed0@gmail.com>
2017-04-21 16:03:59 +0100

    Created Text Editor List
    The text editor list is simply four tuples in a list.

---
+++ test.hs
@@ -0,0 +1,9 @@
+import Data.List
+import System.IO
+
+leftSequence = ("L")
+rightSequence = ("R")
+highlightSequence = ("H")
+bufferSequence = ("B")
+
+textEditor = [leftSequence, rightSequence, highlightSequence, bufferSequence]
