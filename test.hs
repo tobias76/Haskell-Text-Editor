b4d0d978bbf16d155eeb19fff1eaa48c4118c07d
Toby <tobyreed0@gmail.com>
2017-04-21 16:30:10 +0100

    Base of Insert Character

--- test.hs
+++ test.hs
@@ -1,9 +1,17 @@
 import Data.List
 import System.IO
 
+-- Notes
+-- Concatinating is ++
+
+-- These are the sequences we will manipulate
 leftSequence = ("L")
 rightSequence = ("R")
 highlightSequence = ("H")
 bufferSequence = ("B")
 
-textEditor = [leftSequence, rightSequence, highlightSequence, bufferSequence]
+-- This is the text editor itself
+textEditor = [leftSequence, highlightSequence, rightSequence, bufferSequence]
+
+insertCharacter = do
+  leftSequence ++ "Hi"
