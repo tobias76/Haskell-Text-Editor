98acb7a02aa0b99bb9710e62d6e40559783aa861
Toby <tobyreed0@gmail.com>
2017-04-21 16:45:02 +0100

    Base Character Delete

--- test.hs
+++ test.hs
@@ -5,13 +5,19 @@ import System.IO
 -- Concatinating is ++
 
 -- These are the sequences we will manipulate
-leftSequence = ("L")
-rightSequence = ("R")
-highlightSequence = ("H")
-bufferSequence = ("B")
+leftSequence = ("Left")
+rightSequence = ("Right")
+highlightSequence = ("Highlight")
+bufferSequence = ("Buffer")
 
 -- This is the text editor itself
 textEditor = [leftSequence, highlightSequence, rightSequence, bufferSequence]
 
-insertCharacter = do
+-- This concatenates a message onto the left sequence. However I need to allow
+-- parameters to do this.
+characterInsert = do
   leftSequence ++ "Hi"
+
+-- This deletes a character but I need to show the full text editor after
+characterDelete = do
+  drop 1 rightSequence
