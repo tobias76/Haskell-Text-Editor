02e22be989836c7e27bf13a4954119848d8e9f0a
Toby <tobyreed0@gmail.com>
2017-04-21 17:05:13 +0100

    Renamed the source file.
    This file name now makes more sense.

--- Text Editor.hs
+++ Text Editor.hs
@@ -9,15 +9,23 @@ leftSequence = ("Left")
 rightSequence = ("Right")
 highlightSequence = ("Highlight")
 bufferSequence = ("Buffer")
+ri = ("")
 
 -- This is the text editor itself
-textEditor = [leftSequence, highlightSequence, rightSequence, bufferSequence]
+ textEditor = [leftSequence, highlightSequence, rightSequence, bufferSequence]
 
 -- This concatenates a message onto the left sequence. However I need to allow
 -- parameters to do this.
+
+-- TODO: This is not finished
 characterInsert = do
   leftSequence ++ "Hi"
 
 -- This deletes a character but I need to show the full text editor after
 characterDelete = do
-  drop 1 rightSequence
+  tail rightSequence
+
+--TODO: Fix this
+backspace = do
+  reverse leftSequence
+  drop 1 leftSequence
