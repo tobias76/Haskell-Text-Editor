7e6ee3e2bb94dba71ec2d5c96e13020010cec6dc
Toby <tobyreed0@gmail.com>
2017-04-29 12:37:16 +0100

    Added the Line Start and End functions

--- Text Editor.hs
+++ Text Editor.hs
@@ -5,11 +5,19 @@ text :: TextEditor
 text = TextEditor("The cat", " ", "sat on the mat", " ")
 
 -- Define the functions
+lineStart :: TextEditor -> TextEditor
+lineEnd :: TextEditor -> TextEditor
 characterInsert :: Char -> TextEditor -> TextEditor
 characterDelete :: TextEditor -> TextEditor
---backspace :: TextEditor -> TextEditor
+backspace :: TextEditor -> TextEditor
 copy :: TextEditor -> TextEditor
 paste :: TextEditor -> TextEditor
+
+-- Line Start
+lineStart(TextEditor(l, hi, ri, b)) = (TextEditor(" | ", [ ], l ++ " " ++ ri, b))
+
+-- Line End
+lineEnd(TextEditor(l, hi, ri, b)) = (TextEditor(l ++ " " ++ ri, [ ], " | ", b))
 
 -- Character Insert
 characterInsert char (TextEditor(l, hi, r, b)) = (TextEditor(reverse (char: reverse l), hi, r, b))
@@ -18,7 +26,7 @@ characterInsert char (TextEditor(l, hi, r, b)) = (TextEditor(reverse (char: reve
 characterDelete(TextEditor(l, hi, r, b)) = (TextEditor(((l ++  "|" ++ (tail r)), hi, " ", b)))
 
 -- Backspace
-backspace(TextEditor(l, hi, ri, b)) = (TextEditor(reverse (tail(reverse l)), hi, ri, b)) 
+backspace(TextEditor(l, hi, ri, b)) = (TextEditor(reverse (tail(reverse l)), hi, ri, b))
 
 -- Copy
 copy(TextEditor(l, hi, r, b)) = (TextEditor(l, hi, r, hi))
