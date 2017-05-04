31ace93e1c3e206155919d6f07415738c820b1b8
Toby <tobias76@users.noreply.github.com>
2017-05-04 13:51:40 +0100

    More work.

--- Text Editor.hs
+++ Text Editor.hs
@@ -1,3 +1,6 @@
+main :: IO()
+main = return()
+
 data TextEditor = TextEditor ([Char],[Char],[Char],[Char]) deriving(Show)
 
 text :: TextEditor
@@ -12,10 +15,14 @@ lineEnd :: TextEditor -> TextEditor
 characterInsert :: Char -> TextEditor -> TextEditor
 characterDelete :: TextEditor -> TextEditor
 backspace :: TextEditor -> TextEditor
+leftWordMove :: TextEditor -> TextEditor
+rightWordMove :: TextEditor -> TextEditor
 highlightToStart :: TextEditor -> TextEditor
 highlightToEnd :: TextEditor -> TextEditor
 highlightCharacterBefore :: TextEditor -> TextEditor
 highlightCharacterAfter :: TextEditor -> TextEditor
+highlightWordBefore :: TextEditor -> TextEditor
+highlightWordAfter :: TextEditor -> TextEditor
 highlightEverything :: TextEditor -> TextEditor
 copy :: TextEditor -> TextEditor
 paste :: TextEditor -> TextEditor
@@ -48,6 +55,20 @@ characterDelete(TextEditor(l, hi, ri, b)) =
 -- Backspace
 backspace(TextEditor(l, hi, ri, b)) = (TextEditor(reverse (tail(reverse l)), hi, ri, b))
 
+-- Move to the left word (Recursion Attempt)
+-- TODO: Not done
+leftWordMove(TextEditor(l, hi, ri, b)) = (TextEditor(l, hi, ri, b))
+leftWordMove(TextEditor(l, hi, ri, b))
+            | head l == ' ' = (TextEditor(l, hi, ri, b))
+            | otherwise = leftWordMove(TextEditor(l, hi, ri, b))
+
+-- Move to the right word (Recursion Attempt)
+-- TODO: Not done
+rightWordMove(TextEditor(l, hi, ri, b)) = (TextEditor(l, hi, ri, b))
+rightWordMove(TextEditor(l, hi, ri, b))
+            | head ri == ' ' = (TextEditor(l, hi, ri, b))
+            | otherwise = rightWordMove(TextEditor(l, hi, ri, b))
+
 -- Highlight To The Start
 highlightToStart(TextEditor(l, hi, ri, b)) = (TextEditor(" ", l, ri, b))
 
@@ -60,6 +81,18 @@ highlightCharacterBefore(TextEditor(l, hi, ri, b)) = (TextEditor(reverse (tail(r
 -- Highlight Character After
 highlightCharacterAfter(TextEditor(l, hi, ri, b)) = (TextEditor(l, [head ri] , (tail ri), b))
 
+-- Highlight Word Before (Recursion Attempt)
+highlightWordBefore(TextEditor(l, hi, ri, b)) = (TextEditor(l, hi, ri, b))
+highlightWordBefore(TextEditor(l, hi, ri, b))
+                   | head l == ' ' = (TextEditor(l, hi, ri, b))
+                   | otherwise = highlightWordBefore(TextEditor(l, hi, ri, b))
+
+-- Highlight Word After (Recursion Attempt)
+highlightWordAfter(TextEditor(l, hi, ri, b)) = (TextEditor([], hi, ri, b))
+highlightWordAfter(TextEditor(l, hi, ri, b))
+                  | head(reverse ri) == ' ' = (TextEditor(l, hi, ri, b))
+                  | otherwise = highlightWordAfter(TextEditor(l, hi, ri, b))
+                  
 -- Highlight Everything
 highlightEverything(TextEditor(l, hi, ri, b)) = (TextEditor(" ", l ++ ri, " ", b ))
 
@@ -75,8 +108,4 @@ paste(TextEditor(l, hi, ri, b)) =
         else (TextEditor(l, hi, ri, b))
 
 -- Cut
---TODO: Why won't this compile?
-  --cut(TextEditor(l, hi, ri, b)) =
-    --if length hi > 0
-    --  then (TextEditor(l, " ", ri, hi))
-    --   else TextEditor(l, hi, ri, b)
+cut(TextEditor(l, hi, ri, b)) = (TextEditor(l, " ", ri, hi))
